ff00c6227ae02cb0ea532fa8a93ff6695e117ecf
Milin <417156994@qq.com>
2023-03-10 09:25:27 +0800

    simplify docker image.

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
 cd /app
-if [[ "${RUNNING_MODE}" == "http" ]]; then
-    python http_service.py
-else
-    xpra start --no-daemon --html=on --start-child="xterm -e 'python bot.py 2>&1 | tee /tmp/log.txt'"  --exit-with-children
-fi
+python http_service.py
