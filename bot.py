235d22625c0852b069aa66b1742970177c668e86
Milin <417156994@qq.com>
2023-03-23 11:25:42 +0800

    catch all remain exception.

--- bot.py
+++ bot.py
@@ -38,70 +38,72 @@ def ignore_none(value):
 async def handle_message(bot_id: str, message: str, api_version: str = None) -> Tuple[str, str]:
     if not message.strip():
         return config.response.placeholder, ""
+    try:
+        session = chatbot.get_chat_session(bot_id, api_version)
+        # ping
+        if message.strip() in config.trigger.ping_command:
+            return session.get_status(), ""
 
-    session = chatbot.get_chat_session(bot_id, api_version)
-    # ping
-    if message.strip() in config.trigger.ping_command:
-        return session.get_status(), ""
-
-    # 回滚
-    if message.strip() in config.trigger.rollback_command:
-        resp = session.rollback_conversation()
-        if resp:
-            return config.response.rollback_success, get_session_summary(session)
-        return config.response.rollback_fail, get_session_summary(session)
-
-    # 队列满时拒绝新的消息
-    if 0 < config.response.max_queue_size < session.chatbot.queue_size:
-        return config.response.queue_full, get_session_summary(session)
-
-    # 以下开始需要排队
-    async with session.chatbot:
-        try:
-            # 重置会话
-            if message.strip() in config.trigger.reset_command:
-                session.reset_conversation()
-                if session.is_chat_mode():
-                    return config.response.reset_chat.format(system_prompt=session.get_system_prompt()), \
-                           get_session_summary(session)
-                elif session.is_qa_mode():
-                    return config.response.reset_qa, get_session_summary(session)
-                else:
-                    return config.response.reset, get_session_summary(session)
-            if session.is_v3_api():
-                # 聊天模式
-                if message.strip() in config.trigger.chat_command:
-                    session.reset_conversation(interactive_mode=InteractiveMode.CHAT)
-                    return config.response.reset_chat.format(
-                        system_prompt=session.get_system_prompt()), get_session_summary(session)
-                # qa模式
-                if message.strip() in config.trigger.qa_command:
-                    session.reset_conversation(interactive_mode=InteractiveMode.Q_A)
-                    return config.response.reset_qa, get_session_summary(session)
-            # 正常交流
-            resp = await session.get_chat_response(message)
-            interactive_mode_info = "interactive_mode[" + session.interactive_mode.value + "] - " \
-                if session.interactive_mode else ""
+        # 回滚
+        if message.strip() in config.trigger.rollback_command:
+            resp = session.rollback_conversation()
             if resp:
-                session_summary = get_session_summary(session)
-                logger.debug(f"session_id[{bot_id}] - "
-                             f"api_version[{session.api_version}] - "
-                             f"{interactive_mode_info}"
-                             f"chatbot_id[{session.chatbot.id}] - "
-                             f"session_summary{session_summary}")
-                return resp.strip(), session_summary
-        except SSLError as e:
-            logger.exception(e)
-            return config.response.error_network_failure.format(exc=e), get_session_summary(session)
-        except Exception as e:
-            # Other un-handled exceptions
-            if 'Too many requests' in str(e):
-                return config.response.error_request_too_many.format(exc=e), get_session_summary(session)
-            elif 'overloaded' in str(e) or 'Only one message at a time' in str(e):
-                return config.response.error_server_overloaded.format(exc=e), get_session_summary(session)
-            elif 'Unauthorized' in str(e):
-                return config.response.error_session_authenciate_failed.format(exc=e), \
-                       get_session_summary(session)
-            logger.exception(e)
-            return config.response.error_format.format(exc=e), get_session_summary(session)
-    # 排队结束
+                return config.response.rollback_success, get_session_summary(session)
+            return config.response.rollback_fail, get_session_summary(session)
+
+        # 队列满时拒绝新的消息
+        if 0 < config.response.max_queue_size < session.chatbot.queue_size:
+            return config.response.queue_full, get_session_summary(session)
+
+        # 以下开始需要排队
+        async with session.chatbot:
+            try:
+                # 重置会话
+                if message.strip() in config.trigger.reset_command:
+                    session.reset_conversation()
+                    if session.is_chat_mode():
+                        return config.response.reset_chat.format(system_prompt=session.get_system_prompt()), \
+                               get_session_summary(session)
+                    elif session.is_qa_mode():
+                        return config.response.reset_qa, get_session_summary(session)
+                    else:
+                        return config.response.reset, get_session_summary(session)
+                if session.is_v3_api():
+                    # 聊天模式
+                    if message.strip() in config.trigger.chat_command:
+                        session.reset_conversation(interactive_mode=InteractiveMode.CHAT)
+                        return config.response.reset_chat.format(
+                            system_prompt=session.get_system_prompt()), get_session_summary(session)
+                    # qa模式
+                    if message.strip() in config.trigger.qa_command:
+                        session.reset_conversation(interactive_mode=InteractiveMode.Q_A)
+                        return config.response.reset_qa, get_session_summary(session)
+                # 正常交流
+                resp = await session.get_chat_response(message)
+                interactive_mode_info = "interactive_mode[" + session.interactive_mode.value + "] - " \
+                    if session.interactive_mode else ""
+                if resp:
+                    session_summary = get_session_summary(session)
+                    logger.debug(f"session_id[{bot_id}] - "
+                                 f"api_version[{session.api_version}] - "
+                                 f"{interactive_mode_info}"
+                                 f"chatbot_id[{session.chatbot.id}] - "
+                                 f"session_summary{session_summary}")
+                    return resp.strip(), session_summary
+            except SSLError as e:
+                logger.exception(e)
+                return config.response.error_network_failure.format(exc=e), get_session_summary(session)
+            except Exception as e:
+                # Other un-handled exceptions
+                if 'Too many requests' in str(e):
+                    return config.response.error_request_too_many.format(exc=e), get_session_summary(session)
+                elif 'overloaded' in str(e) or 'Only one message at a time' in str(e):
+                    return config.response.error_server_overloaded.format(exc=e), get_session_summary(session)
+                elif 'Unauthorized' in str(e):
+                    return config.response.error_session_authenciate_failed.format(exc=e), \
+                           get_session_summary(session)
+                logger.exception(e)
+                return config.response.error_format.format(exc=e), get_session_summary(session)
+        # 排队结束
+    except Exception as e:
+        return config.response.error_format.format(exc=e), ""
