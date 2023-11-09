917ae9358ad9abcdc1351357fff211c149ee2496
Milin <417156994@qq.com>
2023-11-09 17:40:53 +0800

    feat: add audio param support for http service apis. test

--- utils/speech_to_text.py
+++ utils/speech_to_text.py
@@ -1,15 +1,29 @@
 import speech_recognition as sr
 from loguru import logger
+from werkzeug.datastructures import FileStorage
+import os
+import random
+import string
+
+
+def get_random_name(speech_file):
+    random_name = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    file_extension = os.path.splitext(speech_file.filename)[1]
+    return f'/tmp/{random_name}{file_extension}'
 
 
 def speech_to_text(speech_file):
     logger.info(8)
     if not speech_file:
         return ""
+    file_path = speech_file
+    if isinstance(file_path, FileStorage):
+        file_path = get_random_name(speech_file)
+        speech_file.save(file_path)
     r = sr.Recognizer()
     logger.info(9)
     # 使用AudioFile方法打开音频文件
-    with sr.AudioFile(speech_file) as source:
+    with sr.AudioFile(file_path) as source:
         logger.info(10)
         # 使用record方法录制音频数据
         audio_data = r.record(source)
